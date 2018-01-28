6e841f20848d3ced71edc9e0a7e22acc188269e1
Satendra Dagar <satendra.techmac@gmail.com>
2018-01-28 21:31:03 +0530

    implemented recursive events for FB events.

--- AdvancedCalendar/AdvancedCalendar/FacebookEventManager/FBUserEvent.swift
+++ AdvancedCalendar/AdvancedCalendar/FacebookEventManager/FBUserEvent.swift
@@ -17,6 +17,8 @@ struct FBUserEvent {
     var details:String?
     var eventID:String?
     var rsvp:String?
+    var times :[[String:Any]]?
+    
     
     init(event:Dictionary<String, Any>) {
         //       print(event)
@@ -31,6 +33,9 @@ struct FBUserEvent {
         if let endStr = event["end_time"] as? String {
             end = endStr.dateObject()
         }
+        if let eventTimes = event["event_times"] as? [[String:Any]] {
+            times = eventTimes
+        }
     }
     
     func descirption(){
@@ -38,6 +43,114 @@ struct FBUserEvent {
     }
     func syncEventWithCalendar() {
         
+        if let eventTimings = times{
+            for dict in eventTimings{
+                let startTime = (dict["start_time"] as! String).dateObject()
+                let endTime = (dict["end_time"] as! String).dateObject()
+                let eventID = dict["id"] as! String
+                EKCalendar.syncEventWith(title: title, start: startTime, end: endTime, details: details, eventID: eventID, rsvp: rsvp)
+
+            }
+        }
+        else{
+            EKCalendar.syncEventWith(title: title, start: start, end: end, details: details, eventID: eventID, rsvp: rsvp)
+
+        }
+//        let eventStore = EKEventStore () //default event store
+//        let type = EKEntityType.event;
+//        let calendars : NSArray = eventStore.calendars(for: type) as NSArray
+//        var _calendar : EKCalendar
+//
+//        // 2
+//        let calendarTitle = "Home"
+//        let predicate =  NSPredicate(format:"title matches %@", calendarTitle)
+//        var filtered = calendars.filtered(using: predicate)
+//
+//        if (filtered.count > 0) {
+//            _calendar = filtered[0] as! EKCalendar
+//            print(_calendar.title)
+//            if let id = eventID {//Get event id
+////                let titlePredicate =  NSPredicate(format:"url.absoluteString matches %@",id)
+//                if let startDat = start {
+//                    var endDat = Calendar.current.date(byAdding: .year, value: 1, to: startDat)
+//
+//                    if let endDt = end {
+//                        endDat = endDt
+//                    }
+//                    let pre = eventStore.predicateForEvents(withStart: startDat, end: endDat!, calendars: nil)
+//                    let events = eventStore.events(matching: pre)
+//
+//                    var existingEvent: EKEvent?
+//                    for evt in events{
+//                        if ((evt.url?.absoluteString == "https://www.facebook.com/events/\(id)") || (evt.title == self.title && evt.notes == self.details)){//Check if matching, we just set the url
+//                            existingEvent = evt
+//                            break;
+//                        }
+//
+//                    }
+//                    if let oldEvent = existingEvent{
+//                        oldEvent.title = title ?? "";
+//                        oldEvent.notes = details
+//                        oldEvent.url = URL.init(string: "https://www.facebook.com/events/\(id)")
+//
+//                        if let startDat = start {
+//                            oldEvent.startDate = startDat
+//                        }
+//                        if let endDat = end {
+//                            oldEvent.endDate = endDat
+//                        }
+//                    }
+//                    else{ //Create a new event in the DB
+//                        let newEvent = EKEvent.init(eventStore: eventStore)
+//
+//                        newEvent.title = title ?? "";
+//                        newEvent.notes = details
+//                        newEvent.url = URL.init(string: "https://www.facebook.com/events/\(id)")
+//
+//                        newEvent.calendar = _calendar
+//                        if let startDat = start {
+//                            newEvent.startDate = startDat
+//                            if let endDat = end {
+//                                newEvent.endDate = endDat
+//                            }
+//                            else{
+//                                let endDat = start?.addingTimeInterval(24.0*60.0*60.0)//Assing it for full day
+//                                newEvent.endDate = endDat!
+//                            }
+//                        }
+//
+//                        do {
+//
+//                            try eventStore.save(newEvent, span: .thisEvent, commit: true)
+//                            /* delegate?.eventDidAdd()
+//
+//                             self.dismissViewControllerAnimated(true, completion: nil)*/
+//                        } catch {
+//                            let err = error as NSError
+//                            //                let details = err.localizedDescription
+//                            //                let alert = NSAlert(title: "Event could not save", message: details, preferredStyle: .Alert)
+//                            NSApp.presentError(err)
+//                            print("\((error as NSError).localizedDescription)")
+//
+//                        }
+//
+//                    }
+//                }
+//            }
+//        }
+
+    }
+}
+
+extension EKCalendar{
+    /*var title:String?
+     var start:Date?
+     var end:Date?
+     var details:String?
+     var eventID:String?
+     var rsvp:String?
+ */
+   static func syncEventWith(title:String?, start:Date?,end:Date?,details:String?,eventID:String?,rsvp:String?) {
         let eventStore = EKEventStore () //default event store
         let type = EKEntityType.event;
         let calendars : NSArray = eventStore.calendars(for: type) as NSArray
@@ -52,10 +165,10 @@ struct FBUserEvent {
             _calendar = filtered[0] as! EKCalendar
             print(_calendar.title)
             if let id = eventID {//Get event id
-//                let titlePredicate =  NSPredicate(format:"url.absoluteString matches %@",id)
+                //                let titlePredicate =  NSPredicate(format:"url.absoluteString matches %@",id)
                 if let startDat = start {
                     var endDat = Calendar.current.date(byAdding: .year, value: 1, to: startDat)
-
+                    
                     if let endDt = end {
                         endDat = endDt
                     }
@@ -64,11 +177,11 @@ struct FBUserEvent {
                     
                     var existingEvent: EKEvent?
                     for evt in events{
-                        if ((evt.url?.absoluteString == "https://www.facebook.com/events/\(id)") || (evt.title == self.title && evt.notes == self.details)){//Check if matching, we just set the url
+                        if ((evt.url?.absoluteString == "https://www.facebook.com/events/\(id)") || (evt.title == title && evt.notes == details)){//Check if matching, we just set the url
                             existingEvent = evt
                             break;
                         }
-
+                        
                     }
                     if let oldEvent = existingEvent{
                         oldEvent.title = title ?? "";
@@ -120,6 +233,6 @@ struct FBUserEvent {
                 }
             }
         }
-
     }
 }
+
