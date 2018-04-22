6300b440b9ab2d3d4dc6474fd2626f4224110c07
Satendra Dagar <satendra.techmac@gmail.com>
2018-04-22 09:18:59 +0530

    added google account setting files

---
+++ AdvancedCalendar/AdvancedCalendar/PreferencesController/GoogleAccount/GoogleAccount.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleAccount.swift
+//  AdvancedCalendar
+//
+//  Created by Satendra Dagar on 20/02/18.
+//  Copyright © 2018 Satendra Singh. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class GoogleAccount: NSViewController {
+ 
+    @IBAction func didClickedLogout(_ sender: AnyObject) {
+        print (sender);
+    }
+
+}
